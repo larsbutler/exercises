48ae2a39e1679b4650670e7f788c459924c0e1c8
Lars Butler <Lars.Butler@gmail.com>
2017-12-12 11:49:12 +0100

    Advent of Code 2017, part 2 solution

--- advent-of-code-2017/day7.go
+++ advent-of-code-2017/day7.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	//"reflect"
 	"regexp"
 	"strconv"
 	"strings"
@@ -22,6 +21,19 @@ type Node struct {
 func (node *Node) AddChild(child *Node) {
 	node.Children = append(node.Children, child)
 	child.Parent = node
+}
+
+func (node *Node) TreeWeight() int {
+	var weight int = 0
+	weight += node.Weight
+	for _, child := range node.Children {
+		weight += child.TreeWeight()
+	}
+	return weight
+}
+
+func (node *Node) IsLeaf() bool {
+	return len(node.Children) == 0
 }
 
 func buildTreeTable(lines []string) map[string]*Node {
@@ -70,6 +82,7 @@ func buildTreeTable(lines []string) map[string]*Node {
 	return table
 }
 
+// Turn the soft links from parent to child into hard bi-directional links.
 func buildTree(treeTable map[string]*Node) {
 	for _, node := range treeTable {
 		for _, childName := range node.ChildNames {
@@ -90,6 +103,54 @@ func findTreeRoot(treeTable map[string]*Node) *Node {
 	return nil
 }
 
+func findUnbalancedProgram(root *Node) (minChild *Node, maxChild *Node) {
+	var minWeight int
+	var maxWeight int
+
+	if root.IsLeaf() {
+		minChild = nil
+		maxChild = nil
+		return
+	}
+
+	minWeight = root.Children[0].TreeWeight()
+	maxWeight = minWeight
+	minChild = root.Children[0]
+	maxChild = minChild
+
+	for i := 1; i < len(root.Children); i++ {
+		var child *Node = root.Children[i]
+		var childWeight int
+
+		childWeight = child.TreeWeight()
+
+		if childWeight <= minWeight {
+			minWeight = childWeight
+			minChild = child
+		}
+		if childWeight >= maxWeight {
+			maxWeight = childWeight
+			maxChild = child
+		}
+	}
+	if minWeight != maxWeight {
+		// The maxChild is the unabalanced subtree.
+		// See if there's another node deeper down the tree that is unbalanced,
+		// or if the max child is indeed the unbalanced node.
+		if min, max := findUnbalancedProgram(maxChild); max != nil {
+			minChild = min
+			maxChild = max
+			return
+		} else {
+			// There's nothing deeper; this is the one we're looking for.
+			return
+		}
+	}
+	minChild = nil
+	maxChild = nil
+	return
+}
+
 func solve(lines []string) {
 	var treeTable map[string]*Node
 	var root *Node
@@ -103,6 +164,13 @@ func solve(lines []string) {
 	root = findTreeRoot(treeTable)
 	fmt.Printf("Part 1 solution: %s\n", root.Name)
 
+	var min *Node
+	var max *Node
+	fmt.Printf("Finding the unbalanced program...")
+	min, max = findUnbalancedProgram(root)
+	var diff int = max.TreeWeight() - min.TreeWeight()
+	var solution int = max.Weight - diff
+	fmt.Printf("Part 2 solution: %s needs to be lighter by %d .: %d\n", max.Name, diff, solution)
 }
 
 
